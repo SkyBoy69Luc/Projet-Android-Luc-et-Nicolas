efacaaa2a249d32ca5f72a1ef474e2b1c51ad0dd
Nicolas Bégin <beg83@htomail.fr>
2020-04-14 14:28:05 -0400

    created basedrum

---
+++ app/src/main/java/com/nbegin/projectandroid/Basedrum.java
@@ -0,0 +1,75 @@
+package com.nbegin.projectandroid;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.media.MediaPlayer;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Basedrum extends View {
+
+    private Bitmap baseDrumImage;
+    private MediaPlayer baseDrumSound;
+    private ScaleGestureDetector mScaleDetector;
+
+    public Basedrum(Context context) {
+        super(context);
+    }
+
+    public Basedrum(Context context, @Nullable AttributeSet attrs){
+        super(context);
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        baseDrumImage = BitmapFactory.decodeResource(getResources(), R.raw.basedrum2);
+        baseDrumSound = MediaPlayer.create(context, R.raw.bassdrum);
+        baseDrumSound.setLooping(false);
+
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        @Override
+        public boolean onScale(ScaleGestureDetector detector){
+            invalidate();
+            return true;
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        mScaleDetector.onTouchEvent(event);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                playMediaPlayer();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+        }
+        return true;
+    }
+    private void playMediaPlayer(){
+            baseDrumSound.start();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        drawBitmap(canvas);
+    }
+
+    public void drawBitmap(Canvas canvas) {
+        canvas.save();
+        canvas.scale(0.8f,0.8f);
+        canvas.drawBitmap(baseDrumImage, 100, 100, null);
+        canvas.restore();
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/nbegin/projectandroid/MainActivity.java
+++ app/src/main/java/com/nbegin/projectandroid/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Basedrum basedrum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        basedrum = findViewById(R.id.bassedrum1);
     }
+
+
+
+
+
 }
