5d09e2a7a5f4923a9ebf26226e2060a2e9e6ae08
Nicolas Bégin <beg83@htomail.fr>
2020-04-20 14:15:43 -0400

    added menu & login screen

--- app/src/main/java/com/nbegin/projectandroid/MainActivity.java
+++ app/src/main/java/com/nbegin/projectandroid/MainActivity.java
@@ -2,11 +2,16 @@ package com.nbegin.projectandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Basedrum basedrum;
@@ -15,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     Snare snare;
     Tom1 tom1;
     Tom2 tom2;
+
+    //FirebaseAuth firebaseAuth;
+    EditText emailEdit;
+    EditText passwordEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +43,30 @@ public class MainActivity extends AppCompatActivity {
         tom2 = findViewById(R.id.tom2);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()== R.id.menu_logout){
+            Toast.makeText(this, "Log out",Toast.LENGTH_SHORT).show();
+            //firebaseAuth.signOut();
+            sendUserToSingUpOrLigInActivity();
+            return true;
+        }
+        if(item.getItemId()== R.id.menu_exit){
+            Toast.makeText(this, "Exit",Toast.LENGTH_SHORT).show();
+            //exit
 
+        }
+        return false;
+    }
+    private void sendUserToSingUpOrLigInActivity(){
+        Intent sendUserToSingUpOrLigInIntet = new Intent(this, login.class);
+        startActivity(sendUserToSingUpOrLigInIntet);
+    }
 }
---
+++ app/src/main/java/com/nbegin/projectandroid/login.java
@@ -0,0 +1,35 @@
+package com.nbegin.projectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+        setListener();
+
+    }
+    private void setListener(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToActivity(MainActivity.class);
+            }
+        });
+
+    }
+    private void goToActivity(Class activityToGoTo){
+        Intent sendUserToSingUpOrLigInIntet = new Intent(this, activityToGoTo);
+        startActivity(sendUserToSingUpOrLigInIntet);
+    }
+}
